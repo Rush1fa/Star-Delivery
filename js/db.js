bd2820a81ff13c84ae8dafe46403aa86033a4d4a
Rushifa <158147107+Rush1fa@users.noreply.github.com>
2025-05-09 22:09:52 +0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,6 +1,12 @@
+// Подключение к Supabase
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-export const supabase = createClient(
-  'https://ваш-id.supabase.co',
-  'eleyaqjrlhodkyzxlvcc' // Из настроек проекта в Supabase
-);
+// ! ЗАМЕНИТЕ ЭТИ ДАННЫЕ НА ВАШИ ИЗ SUPABASE !
+const supabaseUrl = 'https://eleyaqjrlhodkyzxlvcc.supabase.co'; // Ваш Project URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsZXlhcWpybGhvZGt5enhsdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MTU0NTUsImV4cCI6MjA2MjM5MTQ1NX0.H2SHCiNpNTZncjWB0tlqjYGeSzXCJL-dFoiHE0jb0GU'; // Ваш anon/public key
+
+// Создаем и экспортируем клиент
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Для теста (опционально)
+console.log("Supabase подключен:", supabase);
