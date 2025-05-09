5badb60a3d803f1f6c9e69d2120a30a24e500f75
Rushifa <158147107+Rush1fa@users.noreply.github.com>
2025-05-09 21:56:06 +0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,5 +2,5 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
 export const supabase = createClient(
   'https://ваш-id.supabase.co',
-  'ваш-public-key' // Из настроек проекта в Supabase
+  'eleyaqjrlhodkyzxlvcc' // Из настроек проекта в Supabase
 );
